179ed9ae2df2ac1f0b64f24f514b106724d1e0f9
samirThaker-DELL <samirthaker2020>
2019-04-14 16:36:35 -0400

    login module -design created

--- app/src/main/java/com/example/group_11_mad3125_project/Controller/Login/Login.java
+++ app/src/main/java/com/example/group_11_mad3125_project/Controller/Login/Login.java
@@ -2,6 +2,7 @@ package com.example.group_11_mad3125_project.Controller.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.group_11_mad3125_project.R;
 
@@ -11,5 +12,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
