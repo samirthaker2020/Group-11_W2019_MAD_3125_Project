a225702789a2aad221a9af43ab96e5a9291a9b1b
samirThaker-DELL <samirthaker2020>
2019-04-14 22:19:34 -0400

    working signup-coding

--- app/src/main/java/com/example/group_11_mad3125_project/Controller/Login/Login.java
+++ app/src/main/java/com/example/group_11_mad3125_project/Controller/Login/Login.java
@@ -1,19 +1,19 @@
 package com.example.group_11_mad3125_project.Controller.Login;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.group_11_mad3125_project.Controller.signup.Signup;
 import com.example.group_11_mad3125_project.R;
-import com.example.group_11_mad3125_project.Splash_screen;
 
 public class Login extends AppCompatActivity {
 private Button btn_signup;
+private TextView txtpassword,txtemail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +22,8 @@ private Button btn_signup;
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         btn_signup=findViewById(R.id.btn_signup);
+        txtemail=findViewById(R.id.txtuid1);
+        txtpassword=findViewById(R.id.txtpass1);
 
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/group_11_mad3125_project/Controller/signup/Signup.java
+++ app/src/main/java/com/example/group_11_mad3125_project/Controller/signup/Signup.java
@@ -4,14 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.group_11_mad3125_project.Controller.Login.Login;
 import com.example.group_11_mad3125_project.R;
 import com.example.group_11_mad3125_project.Splash_screen;
 
 public class Signup extends AppCompatActivity {
-
+private Button btn_cancel;
+private Button btn_register;
+private TextView txtfname;
+private TextView txtlname;
+private TextView txtemailid;
+private TextView txtpassword;
+private TextView txtaddress;
+private TextView txtshipinfo;
+private TextView txtccinfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +32,26 @@ public class Signup extends AppCompatActivity {
 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        btn_cancel=findViewById(R.id.btn_cancel);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginIntent = new Intent(Signup.this,Login.class);
+                startActivity(LoginIntent);
+            }
+        });
+
+        txtaddress=findViewById(R.id.txtaddress);
+        txtemailid=findViewById(R.id.txtuid);
+        txtpassword=findViewById(R.id.txtpass);
+        txtccinfo=findViewById(R.id.txtccinfo);
+        txtshipinfo=findViewById(R.id.txtshipinfo);
+        txtfname=findViewById(R.id.txtfname);
+        txtlname=findViewById(R.id.txtlname);
+
+
+
     }
 
 
